1c37eaa0e941543462fd2e632b9081f773a6a982
CrampedAverage <adnan.aant@gmail.com>
2021-03-22 23:15:04 +0000

    Completed the homepage

--- routes/homepageHandler.js
+++ routes/homepageHandler.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
+const slogans = ['view: Summoners', 'view: Champions', 'view: Builds']
+
 router.get('/', (req, res) => {
     res.render('home', {
         title: "viewLeague || View Players",
         name: 'Home Page',
-        style: "home.css"
+        style: "home.css",
+        slogan: slogans
     })
 })
 
