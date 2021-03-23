419d01f33a09a97f2f5d8ab795b3100cfa30c5b9
CrampedAverage <adnan.aant@gmail.com>
2021-03-23 22:48:03 +0000

    Removed some variables and redundant code

--- routes/champageHandler.js
+++ routes/champageHandler.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
+let name = undefined;
+
+router.get('/', (req, res, next) => {
+
+    next();
+})
+
 router.get('/', (req, res) => {
+    console.log(name)
     res.render('champions', {
         title: "viewLeague || View Champions",
         name: "Champions Page",
--- routes/homepageHandler.js
+++ routes/homepageHandler.js
@@ -1,14 +1,11 @@
-const express = require('express')
-const router = express.Router()
-
-const slogans = ['view: Summoners', 'view: Champions', 'view: Builds']
+const express = require('express');
+const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('home', {
         title: "viewLeague || View Players",
         name: 'Home Page',
-        style: "home.css",
-        slogan: slogans
+        style: "home.css"
     })
 })
 
