869f43f8d1ae8d0fb8ae69f2fc353783f9a920fe
CrampedAverage <adnan.aant@gmail.com>
2021-03-23 22:49:12 +0000

    Added a comment for a middleware to be used in the future

--- routes/champageHandler.js
+++ routes/champageHandler.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-let name = undefined;
-
+// This middleware is responsible for fetching the champions from the API
 router.get('/', (req, res, next) => {
 
     next();
 })
 
 router.get('/', (req, res) => {
-    console.log(name)
     res.render('champions', {
         title: "viewLeague || View Champions",
         name: "Champions Page",
