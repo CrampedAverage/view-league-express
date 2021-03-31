1ce2b11f3e0a9f95e4cc88145f93c61bbe822329
CrampedAverage <adnan.aant@gmail.com>
2021-03-31 23:47:39 +0100

    Fixed the dynamic rendering of player after submitting form

---
+++ routes/playerpageHandler.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const app = express();
+
+let summonerName; 
+
+router.get('/', (req, res, next) => {
+    const path = req.originalUrl.slice(1)
+    summonerName = decodeURI(path)
+
+    next()
+
+})
+
+router.get('/', (req, res) => {
+    res.render('player', {
+        title: `${summonerName} || viewLeague`,
+        name: summonerName,
+        style: "player.css"
+    })
+})
+
+module.exports = router
