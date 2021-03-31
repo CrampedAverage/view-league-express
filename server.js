a25ee56d11bfd702a47a733001f00b4f68cb8f35
CrampedAverage <adnan.aant@gmail.com>
2021-03-31 23:02:04 +0100

    Used instance of exphbs and fixed routing for dynamic player page

--- server.js
+++ server.js
@@ -1,34 +1,30 @@
 require('dotenv').config()
 const express = require('express');
 const exphbs = require('express-handlebars');
+
 const hbs = exphbs.create({
   defaultLayout: 'main',
   layoutsDir: 'views/layouts'
 })
 
+// create application/json parser
+let jsonParser = express.json()
+ 
+// create application/x-www-form-urlencoded parser
+let urlencodedParser = express.urlencoded({ extended: false, limit: '20mb'  })
+
 const app = express();
 const port = process.env.PORT || 4000;
 
-
-// Allows me to read the inputs from the form
-app.use(express.urlencoded({
-  extended: true
-}))
-
 app.use(express.static('public'))
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.engine('handlebars',hbs.engine);
 app.set('view engine', 'handlebars');
 
-
-// 
-app.post('/summoner', (req, res) => {
-  const name = req.body.summoner
-  res.send(name)
-})
-
 // This middleware handles the render of the first page
-app.use('/', require('./routes/homepageHandler'))
+app.use('/', urlencodedParser, require('./routes/homepageHandler'))
+
+app.use('/:id', require('./routes/playerpageHandler'))
 
 // The first middleware is responsible for fetching Champions from riot api
 // and then sending it as a json respons to ./routes/api/champions
@@ -37,9 +33,6 @@ app.use('/champions', require('./middleware/championsData'))
 // The second middleware will fetch the champions from the 
 // ./routes/api/champions then render it
 app.use('/champions', require('./routes/champageHandler'))
-
-// This middleware handles the render of the player match history and stats
-app.use('/player', require('./routes/playerpageHandler'))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
