5acca68f41e7d95b89e64c385f1f12609caa00d9
CrampedAverage <adnan.aant@gmail.com>
2021-04-10 17:51:02 +0100

    Added a variable for region and used dynamic routing depending on region

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require("express");
 const path = require("path");
 const exphbs = require("express-handlebars");
 
+const regions = ['na', 'euw']
+const region = 'euw'
+module.exports = {region, regions}
+
 const hbs = exphbs.create({
     defaultLayout: "main",
     layoutsDir: path.join(__dirname, "views/layouts"),
@@ -24,15 +28,19 @@ app.use(express.static(path.join(__dirname, "public")));
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
+app.get('/', (req, res) => {
+  res.redirect(`${region}`)
+})
+
 // This middleware handles the render of the first page
-app.use("/", urlencodedParser, require("./routes/homepageHandler"));
+app.use(`/${region}`, urlencodedParser, require("./routes/homepageHandler"));
 
 // The second middleware will fetch the champions from the
 // ./routes/api/champions then render it
-app.use("/champions", require("./routes/champageHandler"));
+app.use(`/${region}/champions`, require("./routes/champageHandler"));
 
 // This middlware will be used when the user enters a random route
-app.use("/:id", require("./routes/playerpageHandler"));
+app.use(`/${region}/player/:id`, require("./routes/playerpageHandler"));
 
 app.listen(port, () => {
     console.log(`Example app listening at http://local:${port}`);
