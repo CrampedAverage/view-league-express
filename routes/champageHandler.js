02986010c5b09eeff0b75289dba5a2f9b3a984a0
CrampedAverage <adnan.aant@gmail.com>
2021-04-14 23:55:36 +0100

    Implemented the async function to get list of champions

--- routes/champageHandler.js
+++ routes/champageHandler.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const regionObj = require("../server");
+const API = require('../util/riotAPI') 
+
+let data;
 
 // This middleware is responsible for fetching the champions from the API
-router.get("/", (req, res, next) => {
+router.get("/",  async (req, res, next) => {
+    try {
+        data = await API.championList()
+        data = await Object.values(data.data)
+    }
+    catch(err) {
+        console.log(err)
+    }
     next();
 });
 
@@ -13,6 +23,7 @@ router.get("/", (req, res) => {
         name: "Champions Page",
         style: "champions.css",
         region: regionObj.region,
+        champions: data,
     });
 });
 
