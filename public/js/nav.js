f5c47883ae9ee6b48fe384c8e170d672e7339637
CrampedAverage <adnan.aant@gmail.com>
2021-04-18 22:53:59 +0100

    Added functionality for nav bar closing and opening

---
+++ public/js/nav.js
@@ -0,0 +1,24 @@
+const navSlide = () => {
+    
+    const burger = document.querySelector('.burger')
+    const nav = document.querySelector('.nav-bar')
+    const close = document.querySelector('.close')
+    burger.addEventListener('click', () => {
+        console.log(1)
+        setTimeout(() => {
+            close.classList.toggle('close-active')
+        },500)
+        nav.classList.toggle('nav-active')
+        burger.classList.toggle('burger-inactive')
+    })
+
+    close.addEventListener('click', () => {
+        setTimeout(() => {
+            burger.classList.toggle('burger-inactive')
+        },500)
+        close.classList.toggle('close-active')
+        nav.classList.toggle('nav-active')
+    })
+}
+
+navSlide()
