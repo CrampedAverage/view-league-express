f9ae47e816e4e9d908adcdf86785dfdb9cb01216
CrampedAverage <adnan.aant@gmail.com>
2021-05-09 23:20:35 +0100

    Added an object for the regions abbreviation to code

--- server.js
+++ server.js
@@ -4,7 +4,19 @@ const path = require("path");
 const exphbs = require("express-handlebars");
 const cookieParser = require("cookie-parser");
 
-const regions = ["na", "euw"];
+const regions = {
+    "euw": "euw1",
+    "eune": "eun1",
+    "na": "na1",
+    "jp": "jp1",
+    "lan": "la1",
+    "las": "la2",
+    "oce": "oc1",
+    "kr": "kr",
+    "ru": "ru1",
+    "tr": "tr1",
+    "br": "br1"
+};
 let region;
 module.exports = { region, regions };
 
@@ -32,6 +44,10 @@ app.set("view engine", "handlebars");
 
 app.use("/", urlencodedParser, require("./middleware/homeAction"));
 
+app.use('/error', (req, res) => {
+    res.send("hehe")
+})
+
 // This middleware handles the render of the first page
 app.use(`/:id`, urlencodedParser, require("./routes/homepageHandler"));
 
@@ -41,6 +57,8 @@ app.use(`/:id/champions`, require("./routes/champageHandler"));
 
 // This middlware will be used when the user enters a random route
 app.use(`/:id/player/:id`, require("./routes/playerpageHandler"));
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
