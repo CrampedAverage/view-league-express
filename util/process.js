bf0a7d0e80a789c92b38d6ccf2806d1701adc455
CrampedAverage <adnan.aant@gmail.com>
2021-05-11 23:17:26 +0100

    Added a few more properties to the gameinfo object

--- util/process.js
+++ util/process.js
@@ -1,3 +1,5 @@
+const LeagueStats = require("../helper/leagueStats");
+const Time = require("../helper/Time")
 const riotAPI = require("./riotAPI")
 
 const delay = (ms = 1000) => new Promise(r => setTimeout(r, ms));
@@ -6,8 +8,7 @@ class Process  {
     constructor(userID) {
         this.userID = userID
         this.getGameInfo = this.getGameInfo.bind(this)
-        this.championFilter = this.championFilter.bind(this)
-        this.matchesInfo = this.matchesInfo.bind(this)
+        // this.matchesInfo = this.matchesInfo.bind(this)
     }
     
     championFilter(match) {
@@ -35,19 +36,26 @@ class Process  {
             const playerId =  match.participantIdentities.filter(id => id.player.accountId === this.userID)
 
             const playerInfo = match.participants.filter(player => playerId[0].participantId == player.participantId)
-            // console.log(match)
-            let time = match.gameDuration / 60
-            console.log(time)
-            return {
+
+            const games = {
                 player: playerInfo[0],
                 win: playerInfo[0].stats.win,
+                totalTeamKills: LeagueStats.getTotalKills(match, playerInfo[0]),
                 kills: playerInfo[0].stats.kills,
                 deaths: playerInfo[0].stats.deaths,
                 assists: playerInfo[0].stats.assists,
+                kda: LeagueStats.getKDA(playerInfo[0].stats.kills, playerInfo[0].stats.assists, playerInfo[0].stats.deaths), 
+                kpa: LeagueStats.getKPA(playerInfo[0].stats.kills, LeagueStats.getTotalKills(match, playerInfo[0])),
                 level: playerInfo[0].stats.champLevel,
-                cs: playerInfo[0].stats.totalMinionsKilled,
-                time: match.gameDuration,
-            };
+                cs: playerInfo[0].stats.totalMinionsKilled + playerInfo[0].stats.neutralMinionsKilled,
+                cspm: LeagueStats.getCSPM(
+                    playerInfo[0].stats.totalMinionsKilled + playerInfo[0].stats.neutralMinionsKilled, 
+                    match.gameDuration),
+                time: Time.getTime(match.gameDuration),
+                date: Time.getGameDate(match.gameCreation),
+                timeDiff: Time.calcDiff(match.gameCreation, match.gameDuration)
+            }
+            return games
         })
 
 
