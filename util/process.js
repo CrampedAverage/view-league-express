d0185cc4fbf5d6ef65d421051dadc752863081bf
CrampedAverage <adnan.aant@gmail.com>
2021-05-16 23:03:05 +0100

    Finished the match history object and fixed speed issue for fetchign champion

--- util/process.js
+++ util/process.js
@@ -1,84 +1,91 @@
+const { resolveConfig } = require("prettier");
 const LeagueStats = require("../helper/leagueStats");
-const Time = require("../helper/Time")
-const riotAPI = require("./riotAPI")
+const Time = require("../helper/Time");
+const riotAPI = require("./riotAPI");
 
-const delay = (ms = 1000) => new Promise(r => setTimeout(r, ms));
-
-class Process  {
+class Process {
     constructor(userID) {
-        this.userID = userID
-        this.getGameInfo = this.getGameInfo.bind(this)
-        // this.matchesInfo = this.matchesInfo.bind(this)
-    }
-    
-    championFilter(match) {
-        let champion =  riotAPI.championList()
-        .then(list => {
-            return list.find(champ=> champ.key == match.champion)
-            
-        })
-        .then(champ => {
-
-            return {
-                id: champ.key,
-                name: champ.name,
-                img: champ.image.full
-                
-            }
-        })
-        return champion
+        this.userID = userID;
+        this.getGameInfo = this.getGameInfo.bind(this);
     }
 
+    champion(match) {
+        return match.champion;
+    }
 
     getGameInfo(currentMatch) {
-        const gameInfoForPlayer = riotAPI.match(currentMatch.gameId)
-        .then(match => {
-            const playerId =  match.participantIdentities.filter(id => id.player.accountId === this.userID)
+        const gameInfoForPlayer = riotAPI
+            .match(currentMatch.gameId)
+            .then((match) => {
+                let playerId;
 
-            const playerInfo = match.participants.filter(player => playerId[0].participantId == player.participantId)
+                playerId = match.participantIdentities.filter(
+                    (id) => id.player.accountId === this.userID
+                );
 
-            const games = {
-                player: playerInfo[0],
-                win: playerInfo[0].stats.win,
-                totalTeamKills: LeagueStats.getTotalKills(match, playerInfo[0]),
-                kills: playerInfo[0].stats.kills,
-                deaths: playerInfo[0].stats.deaths,
-                assists: playerInfo[0].stats.assists,
-                kda: LeagueStats.getKDA(playerInfo[0].stats.kills, playerInfo[0].stats.assists, playerInfo[0].stats.deaths), 
-                kpa: LeagueStats.getKPA(playerInfo[0].stats.kills, LeagueStats.getTotalKills(match, playerInfo[0])),
-                level: playerInfo[0].stats.champLevel,
-                cs: playerInfo[0].stats.totalMinionsKilled + playerInfo[0].stats.neutralMinionsKilled,
-                cspm: LeagueStats.getCSPM(
-                    playerInfo[0].stats.totalMinionsKilled + playerInfo[0].stats.neutralMinionsKilled, 
-                    match.gameDuration),
-                time: Time.getTime(match.gameDuration),
-                date: Time.getGameDate(match.gameCreation),
-                timeDiff: Time.calcDiff(match.gameCreation, match.gameDuration)
-            }
-            return games
-        })
+                const playerInfo = match.participants.filter(
+                    (player) =>
+                        playerId[0].participantId == player.participantId
+                );
+                const player = playerInfo[0];
 
-
-        return gameInfoForPlayer
+                const games = {
+                    champion: this.champion(currentMatch),
+                    win: player.stats.win ? "Victory" : "Defeat",
+                    totalTeamKills: LeagueStats.getTotalKills(match, player),
+                    kills: player.stats.kills,
+                    deaths: player.stats.deaths,
+                    assists: player.stats.assists,
+                    kda: LeagueStats.getKDA(
+                        player.stats.kills,
+                        player.stats.assists,
+                        player.stats.deaths
+                    ),
+                    kpa: LeagueStats.getKPA(
+                        player.stats.kills,
+                        LeagueStats.getTotalKills(match, player)
+                    ),
+                    level: player.stats.champLevel,
+                    cs:
+                        player.stats.totalMinionsKilled +
+                        player.stats.neutralMinionsKilled,
+                    cspm: LeagueStats.getCSPM(
+                        player.stats.totalMinionsKilled +
+                            player.stats.neutralMinionsKilled,
+                        match.gameDuration
+                    ),
+                    time: Time.getTime(match.gameDuration),
+                    date: Time.getGameDate(match.gameCreation),
+                    timeDiff: Time.calcDiff(
+                        match.gameCreation,
+                        match.gameDuration
+                    ),
+                    item1: player.stats.item0,
+                    item2: player.stats.item1,
+                    item3: player.stats.item2,
+                    item4: player.stats.item3,
+                    item5: player.stats.item4,
+                    item6: player.stats.item5,
+                };
+                return games;
+            });
+        return gameInfoForPlayer;
     }
 
     async matchesInfo(matchList) {
-
-        const championsPlayed = await Promise.all(
-            matchList.matches.map(this.championFilter)
-        )
-
-        const gameInfo = await Promise.all(
+        let gameInfo = await Promise.all(
             matchList.matches.map(this.getGameInfo)
-        )
+        );
+
+        const version = await Promise.all(
+            matchList.matches.map(riotAPI.getVersion)
+        );
 
         return {
-            championsPlayed,
-            gameInfo
-        }
-        
+            version,
+            gameInfo,
+        };
     }
 }
 
-
-module.exports = Process
+module.exports = Process;
