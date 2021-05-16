9c0d47b30a8d0e55b3622694eeeaadde56597452
CrampedAverage <adnan.aant@gmail.com>
2021-05-16 23:18:52 +0100

    Ran prettier

--- helper/leagueStats.js
+++ helper/leagueStats.js
@@ -1,46 +1,41 @@
 class LeagueStats {
-
     static getKDA(kills, assists, deaths) {
-        let kda = kills + assists
-        kda = kda / deaths
-        kda = kda.toFixed(2)
-        kda = parseFloat(kda)
+        let kda = kills + assists;
+        kda = kda / deaths;
+        kda = kda.toFixed(2);
+        kda = parseFloat(kda);
 
-        return kda
+        return kda;
     }
 
     static getCSPM(cs, rawTime) {
-        let cspm = cs / (rawTime/60)
+        let cspm = cs / (rawTime / 60);
 
-        cspm = cspm.toFixed(1)
-        cspm = parseFloat(cspm)
+        cspm = cspm.toFixed(1);
+        cspm = parseFloat(cspm);
 
-        return cspm
+        return cspm;
     }
-    
+
     static getKPA(kills, totalKills) {
-        let kpa = (kills / totalKills) * 100
-        kpa = kpa.toFixed(0)
-        kpa = parseFloat(kpa)
+        let kpa = (kills / totalKills) * 100;
+        kpa = kpa.toFixed(0);
+        kpa = parseFloat(kpa);
 
-        return kpa
+        return kpa;
     }
-
-
 
     static getTotalKills(match, playerInfo) {
-        const totalTeamKills = match.participants.map(player => {
+        const totalTeamKills = match.participants.map((player) => {
             if (playerInfo.teamId == player.teamId) {
-                return player.stats.kills
+                return player.stats.kills;
             }
-            return 0
-        })
-        const totalKills = totalTeamKills.reduce((acc, cur) => acc + cur)
-    
+            return 0;
+        });
+        const totalKills = totalTeamKills.reduce((acc, cur) => acc + cur);
 
-        return totalKills
+        return totalKills;
     }
-}   
+}
 
-
-module.exports = LeagueStats
+module.exports = LeagueStats;
--- server.js
+++ server.js
@@ -5,17 +5,17 @@ const exphbs = require("express-handlebars");
 const cookieParser = require("cookie-parser");
 
 const regions = {
-    "euw": "euw1",
-    "eune": "eun1",
-    "na": "na1",
-    "jp": "jp1",
-    "lan": "la1",
-    "las": "la2",
-    "oce": "oc1",
-    "kr": "kr",
-    "ru": "ru1",
-    "tr": "tr1",
-    "br": "br1"
+    euw: "euw1",
+    eune: "eun1",
+    na: "na1",
+    jp: "jp1",
+    lan: "la1",
+    las: "la2",
+    oce: "oc1",
+    kr: "kr",
+    ru: "ru1",
+    tr: "tr1",
+    br: "br1",
 };
 let region;
 module.exports = { region, regions };
@@ -44,9 +44,9 @@ app.set("view engine", "handlebars");
 
 app.use("/", urlencodedParser, require("./middleware/homeAction"));
 
-app.use('/error', (req, res) => {
-    res.send("hehe")
-})
+app.use("/error", (req, res) => {
+    res.send("hehe");
+});
 
 // This middleware handles the render of the first page
 app.use(`/:id`, urlencodedParser, require("./routes/homepageHandler"));
@@ -57,8 +57,6 @@ app.use(`/:id/champions`, require("./routes/champageHandler"));
 
 // This middlware will be used when the user enters a random route
 app.use(`/:id/player/:id`, require("./routes/playerpageHandler"));
-
-
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
