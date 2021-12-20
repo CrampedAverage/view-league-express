67346e9ff663e8617d9c4b2319950834c0132410
Adnan Ahmed <adnan.aant@gmail.com>
2021-12-20 01:38:56 +0000

    Added a file to change region to continent value

---
+++ util/continents.js
@@ -0,0 +1,15 @@
+const continents = {
+  euw: "europe", 
+  eune: "europe", 
+  na: "AMERICAS",
+  jp: "ASIA",    
+  lan: "AMERICAS",
+  las: "AMERICAS",
+  oce: "AMERICAS",
+  kr: "ASIA",     
+  ru: "europe",  
+  tr: "europe",  
+  br: "AMERICAS", 
+}
+
+module.exports = continents;
