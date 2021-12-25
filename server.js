1dcfbba8cd70351bd91be230726591851eba22a8
Adnan Ahmed <adnan.aant@gmail.com>
2021-12-25 17:39:46 +0000

    fix: Removed redundant object

--- server.js
+++ server.js
@@ -11,17 +11,6 @@ const hbs = exphbs.create({
   layout: "main",
   partialsDir: path.join(__dirname, "views/partials"),
 });
-const options = {
-  dotfiles: "ignore",
-  etag: false,
-  extensions: ["htm", "html"],
-  index: false,
-  maxAge: "1d",
-  redirect: false,
-  setHeaders: function (res, path, stat) {
-    res.set("x-timestamp", Date.now());
-  },
-};
 
 // create application/json parser
 let jsonParser = express.json();
