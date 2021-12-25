1a78a4c0737da3e5553e1fa8540b4c6b0de64316
Adnan Ahmed <adnan.aant@gmail.com>
2021-12-25 18:47:26 +0000

    feat: Completed game object for game summary

--- dto/game_summary.js
+++ dto/game_summary.js
@@ -1,4 +1,5 @@
 const LeagueStats = require("../helper/LeagueStats");
+const Time = require('../helper/Time')
 
 function game_summary(data, playerIndex) {
   const playerData = data.participants[playerIndex];
@@ -23,8 +24,41 @@ function game_summary(data, playerIndex) {
       playerData.assists
     ),
     level: playerData.champLevel,
-    cs: playerData.neutralMinionsKilled + playerData.totalMinionsKilled,
-    cspm: "",
+    cs: LeagueStats.getCS(playerData.neutralMinionsKilled, playerData.totalMinionsKilled),
+    cspm: LeagueStats.getCS(
+      LeagueStats(playerData.neutralMinionsKilled, playerData.totalMinionsKilled),
+      data.gameDuration
+    ),
+    time: Time.getTime(data.gameDuration),
+    date: Time.getGameDate(data.gameCreation),
+    timeDiff: Time.calcDiff(
+      data.gameCreation,
+      data.gameDuration
+    ),
+    item1: 
+      playerData.item0 === 0
+        ? undefined
+        : playerData.item0,
+    item2:
+      playerData.item1 === 0
+        ? undefined
+        : playerData.item1,
+    item3:
+      playerData.item2 === 0
+        ? undefined
+        : playerData.item2,
+    item4:
+      playerData.item3 === 0
+        ? undefined
+        : playerData.item3,
+    item5:
+      playerData.item4 === 0
+        ? undefined
+        : playerData.item4,
+    item6:
+      playerData.item5 === 0
+        ? undefined
+        : playerData.item5,
   };
 
   return dto;
