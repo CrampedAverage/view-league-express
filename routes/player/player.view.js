193da27c0116846a752699ac81bf6e283d3b91a8
Adnan Ahmed <adnan.aant@gmail.com>
2021-12-29 00:54:14 +0000

    feat: Added a new view function for the player controller

---
+++ routes/player/player.view.js
@@ -0,0 +1,28 @@
+const playerView = (userInfo, userMatches, req) => {
+  const viewObj = {};
+  if (!userInfo.found || userInfo.limitReached) {
+    viewObj.location = 'noUser';
+    viewObj.data = {
+      title: `Not Found || viewLeague`,
+      style: "none.css",
+      region: req.cookies.region,
+    }
+    viewObj.status = 404
+    return viewObj
+  } 
+
+
+  viewObj.location = 'player'
+  viewObj.data = {
+    title: `${userInfo.summonerName} || viewLeague`,
+    name: userInfo.summonerName,
+    style: "player.css",
+    games: userMatches,
+    user: userInfo,
+    region: req.cookies.region,
+  };
+  viewObj.status = 200
+  return viewObj
+}
+
+module.exports = playerView
