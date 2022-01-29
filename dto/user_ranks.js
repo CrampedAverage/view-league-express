0318058815719c134565e01fad86cc59f0c6912a
Adnan Ahmed <adnan.aant@gmail.com>
2022-01-29 17:07:01 +0000

    feat: Added a div to view player rank points

--- dto/user_ranks.js
+++ dto/user_ranks.js
@@ -4,6 +4,7 @@ const user_ranks = (data) => {
   const dto = {
     tier: LeagueStats.capitaliseWord(data.tier),
     rank: data.rank,
+    points: data.leaguePoints,
     wins: data.wins,
     loss: data.losses,
     wr: LeagueStats.getWinrate(data.wins, data.losses),
